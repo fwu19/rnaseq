5745437dd7fcff2d1d8e2bdeac2f11613ed7a75d
Feinan Wu <fwu@fredhutch.org>
2024-10-17 15:44:52 -0700

    modified the script to fetch sample names and fastq files.

--- bin/fqdir_to_csv.sh
+++ bin/fqdir_to_csv.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 echo "sample_id,fastq_1,fastq_2" >fq.csv
+
 for fqdir in $@; do
-	fqdir=$(realpath $fqdir)
-	paste -d "," <(find $fqdir/ | egrep "_S[0-9]+_R1_" | sort | sed "s/.*\///g; s/_S[0-9]\+.*_R1_.*//g" ) <(find $fqdir/ | egrep "_S[0-9]+_R1_" | sort ) <(find $fqdir/ | egrep "_S[0-9]+_R2_" |sort )
-done >>fq.csv
+fqdir=$(realpath $fqdir)
+	paste -d "," <(find $fqdir/ | egrep "_S[0-9]+(_L[0-9]+)?_R1_" | sort | sed "s/.*\///g; s/_S[0-9]\+_R1_.*//; s/_S[0-9]\+_L[0-9]\+_R1_.*//") <(find $fqdir/ | egrep "_S[0-9]+(_L[0-9]+)?_R1_" | sort ) <(find $fqdir/ | egrep "_S[0-9]+(_L[0-9]+)?_R2_" | sort )
+done | egrep -vi "undetermined" >>fq.csv
+
 
