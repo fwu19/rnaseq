a1810969bad8b278beb91bcf957b5f2383027abc
Feinan Wu <fwu@fredhutch.org>
2024-11-21 20:13:06 -0800

    clean up

--- bin/local/json2csv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-json=$1; shift
-csv=$(echo $json | sed "s/json$/csv/")
-
-cat $json | \
-egrep -v "{|}" | \
-sed "s/\"//g; s/[[:space:]]//g; s/,//; s/:/,/" >$csv
-
--- bin/local/subset_fastq.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-outdir=$1; shift
-for fq in "$@"; do
-    zcat $fq | head -n 40000 | gzip -c >$outdir/$(basename $fq)
-done
