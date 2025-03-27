f41ad45bc654bbf63c864a0cdca45f8ee7b46613
Feinan Wu <fwu@fredhutch.org>
2025-03-27 10:11:39 -0700

    update scripts

--- bin/generate_transcript_count_matrix.r
+++ bin/generate_transcript_count_matrix.r
@@ -52,89 +52,6 @@ count2dgelist <- function(counts.tsv=NULL, return.counts = T, pattern2remove="^X
     return(y0)
 }
 
-## PCA
-plot_pca <- function(y, out.prefix, var.genes = NULL, color = NULL, plot.title = '', sample.label = T, feature.length = 'gene_length', scree.plot = F){
-    options(stringsAsFactors = F)
-    require(ggrepel)
-    require(edgeR)
-    
-    ## Create outdir if needed
-    out.dir <- dirname(out.prefix)
-    if(!dir.exists(out.dir)){dir.create(out.dir,recursive = T)}
-    
-    if('chrom' %in% colnames(y$genes)){
-        y <- y[!y$genes$chrom %in% c('chrX', 'chrY', 'chrM'), ]
-    }
-    
-    log2rpkm <- rpkm(y, gene.length = feature.length, normalized.lib.sizes = T, log = T)
-    
-    if (!is.null(var.genes)){
-        keep <- rank(-apply(log2rpkm, 1, var)) < var.genes
-        log2rpkm <- log2rpkm[keep,]
-    }
-    
-    ## Run PCA
-    pca <- prcomp(t(log2rpkm), center = T, scale = T)
-    
-    ## Scree plot
-    pca.variance.prop <- (pca$sdev^2)/sum(pca$sdev^2)*100
-    
-    if (scree.plot){
-        pdf(paste(out.dir,'pca.scree.plot.pdf',sep = '/'), width = 3, height = 3)
-        barplot(
-            pca.variance.prop[1:10],
-            cex.names = 1,
-            xlab = 'Principal component (PC), 1-10',
-            ylab = 'Proportion of variance (%)',
-            main = 'Scree plot',
-            ylim = c(0,100)
-        )
-        
-        points(
-            cumsum(pca.variance.prop)[1:10], col = 'red', type = 'l'
-        )
-        dev.off()
-    }
-    
-    ## PC1 vs PC2
-    df <- cbind(pca$x[,1:2],data.frame(label=rownames(pca$x)))
-    if(is.null(color)){
-        df$color <- y$samples$group
-    }else{
-        df$color <- color
-    }
-    
-    if(length(unique(df$color))>1){
-        p <- ggplot(df,aes(x=PC1,y=PC2,label=label,color=color))
-    }else{
-        p <- ggplot(df,aes(x=PC1,y=PC2,label=label))
-    }
-    p <- p +
-        geom_point(shape = 1)
-    
-    if(sample.label){
-        p <- p +
-            geom_text_repel(size = 2.4, color = 'black', position = 'jitter',max.overlaps = 80)
-    }
-    
-    p <- p +
-        labs(
-            title = plot.title,
-            color = '', 
-            x = paste0('PC1 (',round(pca.variance.prop[1],1),'%)'),
-            y = paste0('PC2 (',round(pca.variance.prop[2],1),'%)')
-        )+
-        theme_bw()+
-        theme(
-            legend.position = 'top'
-        )
-    ggsave(paste(out.prefix,'PCA.pdf',sep='.'),width = 5,height = 5)
-    
-    ## return data
-    return(p)
-    
-}
-
 ## compute normalized counts 
 normalize_counts <- function(y, out.prefix, return = c('rpkm','cpm'), gene.length = "gene_length", log = F){
     require(edgeR)
@@ -192,9 +109,4 @@ y0 <- count2dgelist(
         arrange(factor(id, levels = colnames(cts)[12:ncol(cts)])),
     group.col = 'sample_group'
 )
-
-## plot PCA of all samples ####
-p <- plot_pca(y0, out.prefix = 'all_samples', var.genes = 500, color = y0$samples$group, sample.label = T, feature.length = length.col)
-saveRDS(p, 'pca.rds')
-
 
