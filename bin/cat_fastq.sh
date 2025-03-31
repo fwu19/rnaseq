de8e9a80701c3c83f7d909453de199ed73c122e1
Feinan Wu <fwu@fredhutch.org>
2025-03-30 21:09:14 -0700

    fixed a bug

--- bin/cat_fastq.sh
+++ bin/cat_fastq.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sample_id=$1; shift
+read1_dir=$1; shift
+read2_dir=$1; shift
 
-cat $(find ./ | egrep "S[0-9]+(_L[0-9]+)?_R1_" | sort| tr '\n' ' ' ) > ${sample_id}_merged_R1.fastq.gz
-cat $(find ./ | egrep "S[0-9]+(_L[0-9]+)?_R2_" | sort| tr '\n' ' ' ) > ${sample_id}_merged_R2.fastq.gz
+cat $(find ${read1_dir}/ | sort| tr '\n' ' ' ) > ${sample_id}_merged_R1.fastq.gz
+cat $(find ${read2_dir}/ | sort| tr '\n' ' ' ) > ${sample_id}_merged_R2.fastq.gz
--- modules/cat_fastq.nf
+++ modules/cat_fastq.nf
@@ -10,14 +10,14 @@ process CAT_FASTQ {
     //publishDir "${params.outdir}/merged_fastq/", pattern: '*.fastq.gz', mode: 'copy'
 
     input:
-    tuple val(id), path( "*" ), path( "*" )
+    tuple val(id), path( "read1/*" ), path( "read2/*" )
     
     output:
     tuple val(id), path("${id}_merged_R1.fastq.gz"), path("${id}_merged_R2.fastq.gz"), emit: reads
 
     script:
     """
-    cat_fastq.sh $id
+    cat_fastq.sh $id read1/ read2/
 
     """
 
