d87338b9d965be1340a346f76d4983b106e21017
Feinan Wu <fwu@fredhutch.org>
2025-03-30 21:33:51 -0700

    fixed a bug

--- bin/cat_fastq.sh
+++ bin/cat_fastq.sh
@@ -4,5 +4,5 @@ sample_id=$1; shift
 read1_dir=$1; shift
 read2_dir=$1; shift
 
-cat $(find ${read1_dir}/ | sort| tr '\n' ' ' ) > ${sample_id}_merged_R1.fastq.gz
-cat $(find ${read2_dir}/ | sort| tr '\n' ' ' ) > ${sample_id}_merged_R2.fastq.gz
+cat $(ls ${read1_dir}/* | sort| tr '\n' ' ' ) > ${sample_id}_merged_R1.fastq.gz
+cat $(ls ${read2_dir}/* | sort| tr '\n' ' ' ) > ${sample_id}_merged_R2.fastq.gz
